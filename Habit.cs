e8397693fa22283d7cc823a60bf7783551f21cc5
Engy-Elyamany <engyelyamany26@gmail.com>
2025-10-09 02:24:19 +0300

    push basic version of CreateHabit and ViewHabit

--- Habit.cs
+++ Habit.cs
@@ -1,10 +1,23 @@
 class Habit
 {
-    public string? Habit_Name;
-    public string? Habit_Description;
-    public int Habit_Frequency;
 
-    enum Day
+    private int id;
+    private string Habit_Name;
+    private string Habit_Description;
+    private Day Habit_Frequency;
+
+    static List<Habit> AllHabits = new List<Habit>();
+
+    private Habit(int id, string name, string desc, Day freq)
+    {
+        this.id = id;
+        this.Habit_Name = name;
+        this.Habit_Description = desc;
+        this.Habit_Frequency = freq;
+    }
+
+    [Flags]
+    public enum Day
     {
         SAT = 0b_0000_0001,
         SUN = 0b_0000_0010,
@@ -13,5 +26,26 @@ class Habit
         WED = 0b_0001_0000,
         THU = 0b_0010_0000,
         FRI = 0b_0100_0000,
+    }
+
+    public static Habit CreateHabit(int id, string name, string desc, Day freq)
+    {
+        Habit newHabit = new(id, name, desc, freq);
+        AllHabits.Add(newHabit);
+        return newHabit;
+    }
+
+    public static void ViewAllHabits()
+    {
+        foreach (var Habit in AllHabits)
+        {
+            System.Console.WriteLine(
+            $"Habit ID = {Habit.id}" +
+            $"\nHabit Name = {Habit.Habit_Name}" +
+            $"\nHabit Description = {Habit.Habit_Description}" +
+            $"\nHabit Frequency = {Habit.Habit_Frequency}"
+            );
+            System.Console.WriteLine();
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -2,6 +2,9 @@
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Habit.CreateHabit(1200, "Run", "Running all afternoon", Habit.Day.MON);
+        Habit.CreateHabit(1000, "Code", "Coding", Habit.Day.TUE);
+        Habit.CreateHabit(1010, "Draw", "Drawing", Habit.Day.FRI);
+        Habit.ViewAllHabits();
     }
 }
